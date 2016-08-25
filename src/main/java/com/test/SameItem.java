1af31ae98232173c24392ec4104127ed0d3db451
chamupathi <chamupathi2008@gmail.com>
2016-08-25 17:47:05 +0530

    Chamupathi commit all changes

--- src/main/java/com/test/SameItem.java
+++ src/main/java/com/test/SameItem.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import com.sample.Angle;
 import com.sample.Line;
 import com.sample.Point;
 
@@ -18,11 +19,15 @@ public class SameItem {
 		Line ba = new Line(p2, p1);
 		Line bc = new Line(p2, pc);
 		
+		Angle angleABC = new Angle(ab, bc);
+		Angle angleABCOther = new Angle( bc, ab);
+		
 		
 		
 		System.out.println(p1.sameItem(p3));
 		System.out.println(ab.sameItem(ba));
 		System.out.println(ab.sameItem(bc));
+		System.out.println(angleABC.sameItem(angleABCOther));
 		//System.out.println(ab.getName());
 		//System.out.println(ba.getName());
 
